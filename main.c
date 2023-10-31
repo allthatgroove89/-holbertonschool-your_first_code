fad2c587d437673b4479525765355d90094fcda6
allthatgroove89 <149511005+allthatgroove89@users.noreply.github.com>
2023-10-31 11:45:19 -0400

    My First Code (main.c)

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("School\n");
-  }
-  return 0;
+
+int main() {
+    FILE *file;
+    file = fopen("filename.txt", "r+"); // Open the file for reading and writing
+
+    if (file == NULL) {
+        perror("Error opening the file");
+        return 1;
+    }
+
+    for (int i = 0; i < 32; i++) {
+        fseek(file, 0, SEEK_SET); // Move the file pointer to the beginning
+        fprintf(file, "Hello, School\n"); // Write "Hello, School" to the file 32 times
+    }
+
+    fclose(file); // Close the file
+
+    return 0;
 }
+
+
+
+
+
+
