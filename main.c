80cb0da406862adfee62d5a97c6dbf1ee04129ea
allthatgroove89 <149511005+allthatgroove89@users.noreply.github.com>
2023-10-31 11:53:35 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,4 @@
 
-int main() {
-    FILE *file;
-    file = fopen("filename.txt", "r+"); // 
-
-    if (file == NULL) {
-        perror("Error opening the file");
-        return 1;
-    }
-
-    for (int i = 0; i < 32; i++) {
-        fseek(file, 0, SEEK_SET); // 
-        fprintf(file, "Hello, School\n"); // 
-    }
-
-    fclose(file); // 
-
-    return 0;
-}
 
 
 
